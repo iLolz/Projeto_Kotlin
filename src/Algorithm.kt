52633b8b399917215de95819c10307b2ab3398a3
saviollage <saviohenrique.lage@unifei.edu.br>
2017-11-14 15:26:29 -0200

    Update 0.01

--- src/Algorithm.kt
+++ src/Algorithm.kt
@@ -1,2 +1,16 @@
+import com.sun.tools.doclint.DocLint
 
 open class Algorithm (val description: String, val title: String, val author: String, val version : Int = 1)
+
+
+
+fun main (args: Array<String>)
+{
+     val salg = StringAlgorithm("Just a test", "TOP", "SAVIOLLAGE", 1)
+
+     var aa: String = salg.createPattern()
+     val text: String = "aaaaaaaaaaaaaaaaaaaaaaaaaa"
+
+    println(salg.cesarCypher(text,aa))
+
+}
--- src/StringAlgorithm.kt
+++ src/StringAlgorithm.kt
@@ -1,7 +1,35 @@
+
+import java.util.Random
+
 class StringAlgorithm (description: String, title: String, author: String,  version : Int): Algorithm (description,title,author,version)
 {
 
 
+    fun createPattern(): String {
+
+        val alphabet = "abcdefghijklmnopqrstuvwxyz"
+        val newString = StringBuilder()
+        val rnd = Random()
+
+        while (newString.length < 26) {                   // length of the random string.
+            val index = (rnd.nextFloat() * alphabet.length).toInt()
+            newString.append(alphabet[index])
+        }
+
+        val cypher = newString.toString()
+        return cypher
+
+    }
+
+
+    fun cesarCypher(inputText: String, patternCypher: String) :String
+    {
+
+        var Text: String = ((inputText.toInt() + patternCypher.toInt()) %26).toString()
+
+        return Text
+
+    }
 
 
 }
