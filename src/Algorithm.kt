277566204386c4ee85aee471da667607ef2c9125
Alencar <alencarantonio@unifei.edu.br>
2017-11-19 15:43:19 -0200

    Update 0.0 Update function scheduleExpression

--- src/Algorithm.kt
+++ src/Algorithm.kt
@@ -1,16 +1,43 @@
-import com.sun.tools.doclint.DocLint
+import java.awt.SystemColor.text
+import javax.swing.JOptionPane
 
-open class Algorithm (val description: String, val title: String, val author: String, val version : Int = 1)
+open class Algorithm(val description: String, val title: String, val author: String, val version: Int = 1)
 
 
-
-fun main (args: Array<String>)
-{
-     val salg = StringAlgorithm("Just a test", "TOP", "SAVIOLLAGE", 1)
-
+fun main(args: Array<String>) {
+    val salg = StringAlgorithm("Just a test", "TOP", "SAVIOLLAGE", 1)
+    var op : Int = 0
+    var text : String = "0"
     // var aa: String = salg.createPattern()
-     val text: String = "aaaaaaaaaaaaaaaaaaaaaaaaaabjjjbksdbbjksbdkabkjsd"
-    countOcurrences(text)
- //   println(aa)
 
-}
+    op = JOptionPane.showInputDialog("Digite a opção desejada : \n 1) Count Ocurrences\n 2) Shedule Expression").toInt()
+
+    if (op == 1) {
+        text = JOptionPane.showInputDialog("Digite a String a ser analisada :")
+
+        /// countOcurrences chamada ->  A funçao countOcurrences verifica quantas vezes derterminado caracter aparece na sequencia inserida
+
+        text = countOcurrences(text)
+
+        JOptionPane.showMessageDialog(null, text)
+
+    } else {
+        if (op == 2) {
+            text  = JOptionPane.showInputDialog("Digite a String a ser analisada :").toString()
+
+            text = sheduleExpression(text)
+            JOptionPane.showMessageDialog(null, text)
+            //}
+
+
+            //   println(aa)
+
+
+            System.exit(0)
+
+        } else {
+            JOptionPane.showMessageDialog(null, " Opção Inválida! ")
+            System.exit(0)
+        }
+    }
+2}
--- src/countOcurrences.kt
+++ src/countOcurrences.kt
@@ -1,13 +1,16 @@
-fun countOcurrences(frase: String) {
+import javax.swing.JOptionPane
+
+fun countOcurrences(frase: String) : String {
     var conta: Int = 0
-    var resp: String = "/0"
+    var resp: String = ""
 
     for (x in 'a'..'z') {
         for (i in frase) if (x == i) conta++
 
-        if (conta != 0) print(message = x + "" + conta)
-
-        resp += (x + conta).toString()
+        if (conta != 0) {
+            resp += (conta.toString() + x + " ")
+        }
         conta = 0
     }
+    return resp
 }
