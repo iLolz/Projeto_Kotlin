9b0e1c45c05c2d32e376ed4002d324500ba59454
Saviollage <saviohenrique.lage@unifei.edu.br>
2017-11-28 00:14:44 -0200

    Update
     > Funcao test criada e melhorada

--- src/test/testNumeric.kt
+++ src/test/testNumeric.kt
@@ -5,32 +5,41 @@ import org.testng.Assert
 import org.testng.annotations.Test
 
 class testNumeric {
-
-
     var numeric = NumericAlgorithm("oi","teste","saviollage",1)
-
-
    @Test
     fun testNumeric()
    {
-       var num2String = numeric.shortenerEncode(10)
-       var string2Num = numeric.shortenerDecode("A")
+       var num1 = numeric.shortenerEncode(10)
+       var str1 = numeric.shortenerDecode("A")
+
+       var num2 = numeric.shortenerEncode(350)
+       var str2 = numeric.shortenerDecode("5e")
 
        var stringGenerated = numeric.generateNumberSequence("Teste")
 
-
-
-
+       //TESTE COM VALORES MENORES QUE 62
        Assert.assertEquals(
-               string2Num,
+               str1,
                10
        )
 
        Assert.assertEquals(
-               num2String,
+               num1,
                "A"
        )
 
+
+       //TESTES COM VAORES MAIORES QUE 62
+       Assert.assertEquals(
+               str2,
+               350
+       )
+
+       Assert.assertEquals(
+               num2,
+               "5e"
+       )
+        //TESTE DO HASH
        Assert.assertEquals(
                stringGenerated,
                "Teste".hashCode()
